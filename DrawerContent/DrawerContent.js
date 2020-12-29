5ba51b63b3c2ee95399803942d974dc6cd65a0db
Ayberk Gerey <ayberkgerey@gmail.com>
2020-12-29 08:57:32 +0200

    Add files via upload

---
+++ DrawerContent/DrawerContent.js
@@ -0,0 +1,152 @@
+import * as React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+import {
+  Avatar,
+  Title,
+  Caption,
+  Paragraph,
+  Drawer,
+  Text,
+  TouchableRipple,
+  Switch,
+} from 'react-native-paper';
+import DrawerSection from 'react-native-paper/src/components/Drawer/DrawerSection';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AvatarImage from 'react-native-paper/src/components/Avatar/AvatarImage';
+
+export default function DrawerContent(props) {
+  const [isDarkTheme, setIsDarkTheme] = React.useState(false);
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <DrawerContentScrollView {...props}>
+        <View style={styles.drawerContent}>
+          <View style={styles.userInfoSection}>
+            <View style={{flexDirection: 'row', marginTop: 25}}>
+              <Avatar.Image
+                source={{
+                  uri:
+                    'https://scontent.fsaw1-8.fna.fbcdn.net/v/t1.0-9/130723000_4211508225532570_7042456428318549288_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_ohc=iU8UMBxNvToAX9PoFV9&_nc_ht=scontent.fsaw1-8.fna&oh=36cda3049b68764375781f45211cb566&oe=600EC911',
+                }}
+                size={50}
+              />
+              <View style={{marginLeft: 15, flexDirection: 'column'}}>
+                <Title style={styles.title}>Ayberk Gerey</Title>
+                <Caption style={styles.caption}>@exyphinos</Caption>
+              </View>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.section}>
+                <Paragraph style={[styles.paragraph, styles.caption]}>
+                  80
+                </Paragraph>
+                <Caption style={styles.caption}>Following</Caption>
+              </View>
+              <View style={styles.section}>
+                <Paragraph style={[styles.paragraph, styles.caption]}>
+                  15
+                </Paragraph>
+                <Caption style={styles.caption}>Follower</Caption>
+              </View>
+            </View>
+          </View>
+          <Drawer.Section style={styles.drawerSection}>
+            <DrawerItem
+              icon={({color, size}) => (
+                <Icon name="home-outline" color={color} size={size} />
+              )}
+              label="Home"
+              onPress={() => {
+                props.navigation.navigate('MainScreen');
+              }}
+            />
+            <DrawerItem
+              icon={({color, size}) => (
+                <Icon name="account-outline" color={color} size={size} />
+              )}
+              label="Profile"
+              onPress={() => {
+                props.navigation.navigate('TodoApp');
+              }}
+            />
+            <DrawerItem
+              icon={({color, size}) => (
+                <Icon name="bookmark-outline" color={color} size={size} />
+              )}
+              label="Bookmarks"
+              onPress={() => {}}
+            />
+            <DrawerItem
+              icon={({color, size}) => (
+                <Icon name="account-check-outline" color={color} size={size} />
+              )}
+              label="Support"
+              onPress={() => {}}
+            />
+          </Drawer.Section>
+          <DrawerSection title="Preferences">
+            <TouchableRipple
+              onPress={() => {
+                toggleTheme();
+              }}>
+              <View style={styles.preference}>
+                <Text>Dark Theme</Text>
+                <View pointerEvents="none">
+                  <Switch value={isDarkTheme} />
+                </View>
+              </View>
+            </TouchableRipple>
+          </DrawerSection>
+        </View>
+      </DrawerContentScrollView>
+      <Drawer.Section style={styles.bottomDrawerSection}>
+        <DrawerItem
+          icon={({color, size}) => (
+            <Icon name="exit-to-app" color={color} size={size} />
+          )}
+          label="Sign Out"
+          onPress={() => {}}
+        />
+      </Drawer.Section>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 3,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  row: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  paragraph: {
+    fontWeight: 'bold',
+    marginRight: 3,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+});
